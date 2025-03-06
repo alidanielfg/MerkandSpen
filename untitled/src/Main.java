c0d0229489573be0c365f291e719a97d9c3436fe
alidanielfg <alidan2.3fg@gmail.com>
2025-03-06 11:15:46 -0600

    se agregaron las librerias necesarias para realizar las interfaces y se genero la de usuarios

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        Usuarios usuario1 = new Usuarios();
+
     }
-}
+}//CIERRE MAIN
--- untitled/src/Usuarios.java
+++ untitled/src/Usuarios.java
@@ -1,50 +1,63 @@
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import javax.swing.*;
+
 public class Usuarios {
     private String nombre;
     private String departamento;
     private String rol;
     private String contrasena;
 
-    public Usuarios(String nombre, String departamento, String rol, String contrasena) {
-        this.nombre = nombre;
-        this.departamento = departamento;
-        this.rol = rol;
-        this.contrasena = contrasena;
-
-    }//CIERRE CONSTRUCTOR
-
-
-
-
-    //Zona de gets y sets
-    public String getNombre() {
-        return nombre;
+    public Usuarios() {
+        crearFormulario();
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    private void crearFormulario() {
+        JFrame frame = new JFrame("Registro de Usuario");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FormLayout layout = new FormLayout(
+                "right:pref, 4dlu, fill:pref:g", // Columnas
+                "p, 5dlu, p, 5dlu, p, 5dlu, p, 10dlu, p" // Filas
+        );
+        PanelBuilder builder = new PanelBuilder(layout);
+        CellConstraints cc = new CellConstraints();
+
+        builder.addLabel("Nombre:", cc.xy(1, 1));
+        JTextField nombreField = new JTextField(20);
+        builder.add(nombreField, cc.xy(3, 1));
+
+        builder.addLabel("Departamento:", cc.xy(1, 3));
+        JTextField deptoField = new JTextField(20);
+        builder.add(deptoField, cc.xy(3, 3));
+
+        builder.addLabel("Rol:", cc.xy(1, 5));
+        JTextField rolField = new JTextField(20);
+        builder.add(rolField, cc.xy(3, 5));
+
+        builder.addLabel("Contraseña:", cc.xy(1, 7));
+        JPasswordField passField = new JPasswordField(20);
+        builder.add(passField, cc.xy(3, 7));
+
+        JButton submitButton = new JButton("Guardar");
+        submitButton.addActionListener(e -> {
+            this.nombre = nombreField.getText();
+            this.departamento = deptoField.getText();
+            this.rol = rolField.getText();
+            this.contrasena = new String(passField.getPassword());
+            JOptionPane.showMessageDialog(frame, "Usuario registrado con éxito.");
+        });
+        builder.add(submitButton, cc.xyw(1, 9, 3));
+
+        frame.add(builder.getPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Usuarios::new);
     }
 }//CIERRE USUARIOS
